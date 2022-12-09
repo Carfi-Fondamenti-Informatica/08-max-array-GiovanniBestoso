bbb70df49e6676b1b75f55389d3def9f6493fa44
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-12-09 18:27:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int n=0;cin>>n;
+    float numeri[n];
+    for (int i = 0; i < n; i++) {
+        cin>>numeri[i];
+    }
+    float x=0;
+    for (int i = 0; i < n-1;i++) {
+        float y= nmaggiore(numeri[i], numeri[i+1]);
+        if (y>x){
+            x=y;
+        }
+    }cout<<x<<endl;
   return 0;
 }
